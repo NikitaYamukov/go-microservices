660f51bf7ca86caa3392279f0e9f1c889f2834de
Nikita <nikita@air-nikita.lan>
2026-05-14 17:07:02 -0400

    Add user models and repository methods

---
+++ internal/repository/mapper/mapper.go
@@ -0,0 +1,68 @@
+package mapper
+
+import (
+	"github.com/NikitaYamukov/go-microservices/internal/model"
+	repomodel "github.com/NikitaYamukov/go-microservices/internal/repository/model"
+)
+
+func UserToRepoUser(user model.User) repomodel.User {
+	return repomodel.User{
+		ID:         user.ID,
+		Login:      user.Login,
+		Email:      user.Email,
+		Phone:      user.Phone,
+		FirstName:  user.FirstName,
+		LastName:   user.LastName,
+		MiddleName: user.MiddleName,
+		Age:        user.Age,
+		CreatedAt:  user.CreatedAt,
+		UpdatedAt:  user.UpdatedAt,
+	}
+}
+
+func RepoUserToUser(user repomodel.User) model.User {
+	return model.User{
+		ID:         user.ID,
+		Login:      user.Login,
+		Email:      user.Email,
+		Phone:      user.Phone,
+		FirstName:  user.FirstName,
+		LastName:   user.LastName,
+		MiddleName: user.MiddleName,
+		Age:        user.Age,
+		CreatedAt:  user.CreatedAt,
+		UpdatedAt:  user.UpdatedAt,
+	}
+}
+
+func RepoUsersToUsers(users []repomodel.User) []model.User {
+	res := make([]model.User, len(users))
+	for i, user := range users {
+		res[i] = RepoUserToUser(user)
+	}
+
+	return res
+}
+
+func CreateUserToRepoUser(user model.CreateUser) repomodel.User {
+	return repomodel.User{
+		Login:      user.Login,
+		Email:      user.Email,
+		Phone:      user.Phone,
+		FirstName:  user.FirstName,
+		LastName:   user.LastName,
+		MiddleName: user.MiddleName,
+		Age:        user.Age,
+	}
+}
+
+func UpdateUserToRepoUser(user model.UpdateUser) repomodel.User {
+	return repomodel.User{
+		Email:      user.Email,
+		Phone:      user.Phone,
+		FirstName:  user.FirstName,
+		LastName:   user.LastName,
+		MiddleName: user.MiddleName,
+		Age:        user.Age,
+	}
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,8 +1,16 @@
 package repository
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/NikitaYamukov/go-microservices/internal/model"
+	"github.com/NikitaYamukov/go-microservices/internal/repository/mapper"
+	repomodel "github.com/NikitaYamukov/go-microservices/internal/repository/model"
 	"github.com/rs/zerolog"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type Repository struct {
@@ -15,4 +23,77 @@ func NewRepository(db *gorm.DB, logger zerolog.Logger) *Repository {
 		db:     db,
 		logger: logger,
 	}
+}
+
+func (r *Repository) CreateUser(ctx context.Context, user model.CreateUser) error {
+	userRepo := mapper.CreateUserToRepoUser(user)
+	res := r.db.WithContext(ctx).
+		Clauses(clause.OnConflict{UpdateAll: true}).
+		Create(&userRepo)
+	if res.Error != nil {
+		r.logger.Err(res.Error).Msg("failed to save user")
+		return fmt.Errorf("failed to save user: %w", res.Error)
+	}
+
+	return nil
+}
+
+func (r *Repository) GetUser(ctx context.Context, userID uint64) (model.User, error) {
+	var user repomodel.User
+	res := r.db.WithContext(ctx).
+		Model(&repomodel.User{}).
+		Where("id = ?", userID).
+		First(&user)
+
+	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+		return model.User{}, fmt.Errorf("user not found")
+	} else if res.Error != nil {
+		r.logger.Err(res.Error).Msg("failed to get user")
+		return model.User{}, fmt.Errorf("failed to get user: %w", res.Error)
+	}
+
+	return mapper.RepoUserToUser(user), nil
+}
+
+func (r *Repository) GetUsers(ctx context.Context, limit int, offset int) ([]model.User, error) {
+	var users []repomodel.User
+	res := r.db.WithContext(ctx).
+		Model(&repomodel.User{}).
+		Offset(offset).
+		Limit(limit).
+		Find(&users)
+
+	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+		return nil, fmt.Errorf("users not found")
+	} else if res.Error != nil {
+		r.logger.Err(res.Error).Msg("failed to get users")
+		return nil, fmt.Errorf("failed to get users: %w", res.Error)
+	}
+
+	return mapper.RepoUsersToUsers(users), nil
+}
+
+func (r *Repository) DeleteUser(ctx context.Context, userID uint64) error {
+	res := r.db.WithContext(ctx).
+		Where("id = ?", userID).
+		Delete(&repomodel.User{})
+	if res.Error != nil {
+		r.logger.Err(res.Error).Msg("failed to delete user")
+		return fmt.Errorf("failed to delete user: %w", res.Error)
+	}
+
+	return nil
+}
+
+func (r *Repository) UpdateUser(ctx context.Context, userID uint64, user model.UpdateUser) error {
+	userRepo := mapper.UpdateUserToRepoUser(user)
+	res := r.db.WithContext(ctx).
+		Where("id = ?", userID).
+		Updates(userRepo)
+	if res.Error != nil {
+		r.logger.Err(res.Error).Msg("failed to update user")
+		return fmt.Errorf("failed to update user: %w", res.Error)
+	}
+
+	return nil
 }
